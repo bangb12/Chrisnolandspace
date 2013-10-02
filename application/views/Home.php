b0cfea2eb99fcbd03bce87caa54c57c6509d9746
Christopher Noland <bangb12@fullsail.edu>
2013-10-02 11:30:29 -0400

    Created file structure, added Bootstrap and Codeigniter

---
+++ application/views/Home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>FAQ - ImageBase</title>
+    <link href="../../css/bootstrap/css/bootstrap.css" rel="stylesheet"></script>
+</head>
+<body>
+    
+</body>
+</html>
