b875712eecb5d263711cd682fcac4bf1bfa86eec
Christopher Noland <bangb12@fullsail.edu>
2013-10-02 11:42:49 -0400

    Home structure complete

--- application/views/Home.php
+++ application/views/Home.php
@@ -3,10 +3,30 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-    <title>FAQ - ImageBase</title>
-    <link href="../../css/bootstrap/css/bootstrap.css" rel="stylesheet"></script>
+    <title>Home - ChrisNoland'sSpace</title>
+    <link href="../../css/bootstrap.css" rel="stylesheet"></script>
 </head>
 <body>
-    
+    <div class="container-fluid">
+        <header>
+            <img src="" alt="Chris Noland's Space Logo" />
+            <ul>
+                <li><h3><a>Home</a></h3></li>
+                <li><h3><a>Portfolio</a></h3></li>
+                <li><h3><a>About</a></h3></li>
+            </ul>
+        </header>
+        <section>
+            <div id="Featured">
+                <img src="" alt="" />
+                <h1>Project</h1>
+                <p>Project Description</p>
+                <p><a></a></p>
+            </div>
+        </section>
+        <footer>
+            <p></p>
+        </footer>
+    </div>
 </body>
 </html>
